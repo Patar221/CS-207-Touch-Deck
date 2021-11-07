672622bbee077365d68f4222b2babeafb3f13483
Patrick <leblancpatrick21@gmail.com>
2021-11-07 11:01:35 -0600

    Added cases for all buttons
    Each button will just print the button number

--- Touch-Deck-Desktop/SerialRead.py
+++ Touch-Deck-Desktop/SerialRead.py
@@ -15,7 +15,38 @@ while True:
                 print(command)
                 webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
             case '2':
-                print(command)
                 subprocess.call(['C:\Program Files (x86)\Steam\steam.exe'])
+            case '3':
+                print(command)
+            case '4':
+                print(command)
+            case '5':
+                print(command)
+            case '6':
+                print(command)
+            case '7':
+                print(command)
+            case '8':
+                print(command)
+            case '9':
+                print(command)
+            case '10':
+                print(command)
+            case '11':
+                print(command)
+            case '12':
+                print(command)
+            case '13':
+                print(command)
+            case '14':
+                print(command)
+            case '15':
+                print(command)
+            case '16':
+                print(command)
+            case '17':
+                print(command)
+            case '18':
+                print(command)
             case _:
                 print("No Command Found")
