d61b5d02f8e8af1b294e50cf7fe570204238d100
Patrick <leblancpatrick21@gmail.com>
2021-11-29 10:18:20 -0600

    Changed code to read from json

--- Touch-Deck-Desktop/SerialRead.py
+++ Touch-Deck-Desktop/SerialRead.py
@@ -3,63 +3,41 @@ import re
 import webbrowser
 import subprocess
 import keyboard
+import json
 
-def openBrwoser(url):
+# Functions for each command option
+def openBrowser(url):
     webbrowser.open(url)
 
 def openFile(path):
     subprocess.call(path)
 
 def sendKey(keys):
-  keyboard.send(keys)
+    keyboard.send(keys)
 
 def typeText(text):
     keyboard.write(text)
 
+def typeTextAndSend(text):
+    keyboard.write(text)
+    keyboard.send('enter')
+
+# Open serial connection to Arduino
 ser = serial.Serial('COM5', 9600)
 
+# Load JSON config
+with open('config.json') as f:
+  config = json.load(f)
+
+# Infinite loop to read input from Arduino
 while True:
     line = ser.readline().decode('utf-8')
     touchDeckCommands = re.findall(r'(?<=touchDeck\()(.*?)(?=\))', line)
 
     for button in touchDeckCommands:
-        match button:
-            case '1':
-                print(button)
-                openBrwoser("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-            case '2':
-                openFile(['C:\Program Files (x86)\Steam\steam.exe'])
-            case '3':
-                print(button)
-            case '4':
-                print(button)
-            case '5':
-                print(button)
-            case '6':
-                print(button)
-            case '7':
-                typeText('Hello')
-            case '8':
-                print(button)
-            case '9':
-                print(button)
-            case '10':
-                print(button)
-            case '11':
-                print(button)
-            case '12':
-                print(button)
-            case '13':
-                sendKey('shift+F11')
-            case '14':
-                sendKey('shift+F12')
-            case '15':
-                print(button)
-            case '16':
-                print(button)
-            case '17':
-                print(button)
-            case '18':
-                print(button)
-            case _:
-                print("No Command Found")
+        # Load command information from json
+        command = config[button]['command']
+        arg = config[button]['argument']
+
+        # Call the command loaded from the json file
+        eval(command + f"('{arg}')")
